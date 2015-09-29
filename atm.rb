ec121307a324223be41fd5818f74dcb5bf28a444
Speaslee <sophiapeaslee@gmail.com>
2015-09-29 09:54:09 -0400

    new edits

--- atm.rb
+++ atm.rb
@@ -21,7 +21,6 @@ class ATM
       users<<u
     end
     aut_pin
-    menu_options
   end
 
   def aut_pin
@@ -33,60 +32,59 @@ class ATM
     self.current_user = users.find.each { |user| user.name == user_input && user.pin == pin_input }
 
     unless current_user
-      puts "Please try entering your information again"
+      puts "Please try entering your information again. Remember your name is case sensitive. "
       aut_pin
     end
-  end
-
-
-def menu_options
-  puts "pick an option"
-  puts "Please select 1 to withdraw
-  Please select 2 to check balance
-  Please select 3 to exit."
-  s=gets.chomp
-  if s == "1"
-    withdraw
-  elsif s == "2"
-    check_balance
-  elsif s == "3"
-    ATM.new
-  else puts "Please try a different selection"
     menu_options
   end
-end
 
-def withdraw
-  puts "How much would you like to withdraw?"
-  w= gets.chomp
-  current_user.balance -= w.to_i
-  puts "Would you like to do anything else? Please type yes or no:"
-    answer= gets.chomp
-    if answer == "yes"
-      menu_options
-    elsif answer == "no"
-      ATM.new
-    else puts "Please type yes or no."
+
+  def menu_options
+    puts "Pick an option"
+    puts "Please select 1 to withdraw:
+    Please select 2 to check balance:
+    Please select 3 to exit:"
+    s=gets.chomp
+    if s == "1"
       withdraw
-    end
-end
-
-def check_balance
-  puts "Your balance is #{current_user.balance}"
-  puts "Would you like to do anything else? Please type yes or no:"
-    answer= gets.chomp
-    if answer == "yes"
-      menu_options
-    elsif answer == "no"
-      ATM.new
-    else puts "Please type yes or no."
+    elsif s == "2"
       check_balance
+    elsif s == "3"
+      aut_pin
+    else puts "Invalid Entry. Please try a different selection:"
+      menu_options
     end
   end
 
+  def withdraw
+    puts "How much would you like to withdraw?"
+    w= gets.chomp
+    current_user.balance -= w.to_i
+    options
+  end
+
+  def check_balance
+    puts "Your balance is #{current_user.balance}"
+    options
+    end
+  end
+
+def options
+  puts "Would you like to do anything else? Please type yes or no:"
+  answer= gets.chomp
+  if answer.downcase == "yes"
+    menu_options
+  elsif answer.downcase == "no"
+    aut_pin
+  else puts "Invalid Entry. Please type yes or no:"
+    options
+  end
 end
+
 
 class User
   attr_accessor :name, :pin, :balance
 end
+
+
 my_atm= ATM.new
